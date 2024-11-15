415147e55042604efa08262fb4a83498f14c2ff9
dbryant <d.bryant718@gmail.com>
2024-11-15 10:17:23 -0500

    setup server

---
+++ app.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+const username = prompt('What is your name? ');
+
+console.log(`Your name is ${username}`);
+
+// let createCustomer = ;
+
+// let viewCustomer = ;
+
+// let updateCustomer = ;
+
+// let deleteCustomer = ;
+
+// let quit = ;
+
