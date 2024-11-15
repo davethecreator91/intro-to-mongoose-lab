f658d3e0cac1e00a605d8e00ad4a6409cafe6635
dbryant <d.bryant718@gmail.com>
2024-11-15 11:59:32 -0500

    added customer

--- queries.js
+++ queries.js
@@ -30,3 +30,72 @@ const runQueries = async () => {
 
 connect()
 /*------------------------------ Query Functions -----------------------------*/
+
+const CustomerCRMSchema = new mongoose.Schema({
+id: { type:mongoose.Schema.Types.ObjectId, /*required: true */ },
+name: { type: String, required: true},
+age: { type: Number, required:true}
+});
+
+// Compile the schema into a model:
+const Customer = mongoose.model('Customer', CustomerCRMSchema);
+
+// Export the model:
+module.exports = Customer;
+
+
+
+//Customer Data Management
+
+// const Customer = require('./queries');
+
+const prompt = require('prompt-sync')();
+
+const username = prompt('What is your name? ');
+
+console.log(`Your name is ${username}`);
+
+// menu
+
+async function mainMenu() {
+    while (true) {
+        console.log('\n=== Customer Management ===');
+        console.log('1. Add a customer');
+        console.log('2. View all customers');
+        console.log('3. Update a customer');
+        console.log('4. Delete a customer');
+        console.log('5. Exit');
+        const choice = prompt('Choose an option (1-5): ')
+
+        switch (choice) {
+            case '1':
+                await addCustomer();
+                break;
+            case '2':
+                await viewCustomers();
+                break;
+            case '3':
+                await updateCustomer();
+                break;
+            case '4':
+                await deleteCustomer();
+                break;
+            case '5':
+                console.log('Exiting...');
+                mongoose.connection.close();
+                return;
+            default:
+                console.log('Invalid choice. Please try again.'); 
+        }
+    }
+}
+mainMenu();
+async function addCustomer() {
+    const name = prompt('Enter a customer name ');
+    const age = parseInt(prompt('Enter a customer age'), 10);
+    const customer = new Customer({ name, age });
+    await customer.save();
+    console.log('Customer added!');
+    
+}
+
